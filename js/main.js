c61aa61188e0a0a127651db3a9d3c60b7846aec4
Mingchao Zhang <mz5@illinois.edu>
2019-02-19 20:16:36 -0600

    final almost

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ function hide_modal() {
 }
 
 function display_modal() {
-    console.log("DISPLAY!");
     modal.style.display = "block";
 }
 
@@ -27,10 +26,8 @@ function modal_show_pic(evt) {
     display_modal();
 }
 
-console.log(modal_pics.length);
 
 for (var i = 0; i < modal_pics.length; i++) {
-    console.log(i);
     modal_pics[i].addEventListener("click", modal_show_pic);
 }
 
@@ -56,13 +53,79 @@ window.addEventListener("scroll", resize_navbar);
 // scrolling progress indicator
 var progress_bar = document.getElementById("progress_bar");
 function fill_progress_bar() {
-    console.log("hello");
     const total_scroll_length = document.documentElement.scrollHeight - window.innerHeight;
     var scrolled_percentage = (window.scrollY / total_scroll_length) * 100;
     progress_bar.style.width = scrolled_percentage + "%";
 }
 window.addEventListener("scroll", fill_progress_bar);
+//---------------------------------------------------
+// hightlight anchor links
+var home_height = document.getElementById("home").offsetHeight;
+var about_height = document.getElementById("about").offsetHeight;
+var nature_height = document.getElementById("nature").offsetHeight;
+var food_height = document.getElementById("food").offsetHeight;
+var footer_height = document.getElementById("footer").offsetHeight;
 
+
+
+var nav_highlight_index = 0;
+var nav_anchor_links = document.getElementsByClassName("nav_anchor_link");
+nav_anchor_links[nav_highlight_index].classList.add("anchor_link_highlight");
+
+function change_obj_color(obj, background_color, color) {
+    obj.style.backgroundColor = background_color;
+    obj.style.color = color;
+}
+
+var nav_white = "rgba(255, 255, 255, 0.8)";
+var nav_black = "rgba(0, 0, 0, 0.8)";
+
+function highlight_anchor_link() {
+    var home_top = 0;
+var home_bot = home_height - navbar.clientHeight - 1;
+var about_top = home_bot + 1;
+var about_bot = about_top + about_height - 1;
+var nature_top = about_bot + 1;
+var nature_bot = nature_top + nature_height - 1;
+var food_top = nature_bot + 1;
+var food_bot = food_top + food_height - 1;
+var footer_top = food_bot + 1;
+var footer_bot = food_top + food_height;
+
+    for (var i = 0; i < nav_anchor_links.length; i++) {
+        console.log(nav_anchor_links[i].offsetTop);
+        change_obj_color(nav_anchor_links[i], nav_white, nav_black);
+    }
+    if (home_top <= window.scrollY &&
+        window.scrollY <= home_bot) {
+        change_obj_color(nav_anchor_links[0], nav_black, nav_white);
+        //nav_anchor_links[0].classList.add("anchor_link_highlight");
+    }
+    else if (about_top <= window.scrollY &&
+        window.scrollY <= about_bot) {
+        change_obj_color(nav_anchor_links[1], nav_black, nav_white);
+    }
+    else if (nature_top <= window.scrollY &&
+        window.scrollY <= nature_bot) {
+        change_obj_color(nav_anchor_links[2], nav_black, nav_white);
+    }
+    else if (food_top <= window.scrollY &&
+        window.scrollY <= document.documentElement.scrollHeight - window.innerHeight - navbar.clientHeight) {
+        change_obj_color(nav_anchor_links[3], nav_black, nav_white);
+    }
+    else {
+        change_obj_color(nav_anchor_links[4], nav_black, nav_white);
+    }
+    console.log(food_top + navbar.clientHeight - 1, window.scrollY, food_top, food_bot);
+}
+
+
+window.addEventListener("scroll", highlight_anchor_link);
+/*
+
+*/
+
+console.log(home_height, about_height, nature_height, food_height, footer_height);
 //---------------------------------------------------
 // Carousel
 var front_images = document.getElementsByClassName("nature_front_image");
